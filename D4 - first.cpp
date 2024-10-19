9581962bef4545377828e91e4380c494915a9729
Nisala Cooray <cooraynisala@gmail.com>
2024-10-19 19:31:17 +0530

    Add files via upload
    Let the user insert a string value and find the length of the characters using length tag

---
+++ D4 - first.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    string a,b;
+    cout<<"Please enter the string: ";getline(cin,a);
+    int n= a.length();
+
+    cout<<"The Number of characters in the string is: "
+    <<n;
+}
