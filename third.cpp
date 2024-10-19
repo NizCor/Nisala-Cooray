7c0ff4608808863a4fe4f8768ae5085d142c1929
Nisala Cooray <cooraynisala@gmail.com>
2024-10-19 18:35:39 +0530

    Add files via upload
    Types of Variables in C++ language

--- third.cpp
+++ third.cpp
@@ -2,7 +2,40 @@
 using namespace std;
 int main()
 {
-    cout<<"Nisala Cooray" <<endl;
-    cout<<"my age is 21";
+    int _a;
+    double a;
+    float A;
+    char a_;
+    string Ab;
+    bool AB;
+
+    cout<<"Enter the integer: ";
+    cin>>_a;
+    cout<<endl;
+    cout<<"Enter the double Number: ";
+    cin>>a;
+    cout<<endl;
+    cout<<"Enter the float number: ";
+    cin>>A;
+    cout<<endl;
+    cout<<"Enter the character: ";
+    cin>>a_;
+    cout<<endl;
+    cout<<"Enter the string :";
+    cin>>Ab;
+    cout<<endl;
+    cout<<"Enter the boolean :";
+    cin>>AB;
+    cout<<endl;
+    cout<<"int is: "<<_a<<endl;
+    cout<<"double is: "<<a<<endl;
+    cout<<"float is: "<<A<<endl;
+    cout<<"char is: "<<a_<<endl;
+    cout<<"string is: "<<Ab<<endl;
+    cout<<"bool is: "<<AB<<endl;
     return 0;
+
+
+
+
 }
