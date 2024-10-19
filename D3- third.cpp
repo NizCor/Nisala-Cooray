69f88c229958c132de92f074dd841e0ad01b6acc
Nisala Cooray <cooraynisala@gmail.com>
2024-10-19 19:29:39 +0530

    Add files via upload
    To find the Area & the Perimeter of a Rectangle using mathematical calculations

---
+++ D3- third.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+   double A,B;
+   cout<<"enter the length :";
+   cin>>A;
+
+   cout<<"enter the width :";
+   cin>>B;
+
+   cout<<"Area is :"<<(A*B)<<endl;
+   cout<<"Perimeter is :"<<(A+B)*2<<endl;
+   return 0;
+}
