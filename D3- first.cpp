82d820df0695677cc7acbd47ae849181827a7f14
Nisala Cooray <cooraynisala@gmail.com>
2024-10-19 19:27:18 +0530

    Add files via upload
    Mathematical Calculations

---
+++ D3- first.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int x,y;
+    cout<<"Please enter 2 numbers " <<endl;
+    cin>>x>>y;
+
+    cout<<"x+y= "<<(x+y) <<endl;
+    cout<<"x-y= "<<(x-y) <<endl;
+    cout<<"x*y= "<<(x*y) <<endl;
+    cout<<"x/y= "<<(x/y) <<endl;
+    cout<<"x%y= "<<(x%y) <<endl;
+    cout<<"x++= "<<(x++) <<endl;
+    cout<<"++x= "<<(++x) <<endl;
+    cout<<"y++= "<<(y++) <<endl;
+    cout<<"++y= "<<(++y) <<endl;
+
+    return 0;
+}
